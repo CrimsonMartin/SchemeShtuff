f6d4930dbb2c25cae531c8defe45041d1344d671
Michael Folz <maf152@case.edu>
2018-04-01 17:27:43 -0400

    removed wrong test files, and working on interpreter3

--- interpreter3/interpreter3.scm
+++ interpreter3/interpreter3.scm
@@ -8,8 +8,6 @@
 
 
 ; An interpreter for the c-ish language that uses call/cc for the continuations.  Does not handle side effects.
-(define call/cc call-with-current-continuation)
-
 
 ; The functions that start interpret-...  all return the current environment.
 ; The functions that start eval-...  all return a value
@@ -60,50 +58,44 @@
         (insert (get-declare-var statement) 'novalue environment))))
 
 ; Updates the environment to add an new binding for a variable
-(define interpret-assign
-  (lambda (statement environment)
-    (update (get-assign-lhs statement) (eval-expression (get-assign-rhs statement) environment) environment)))
+(define (interpret-assign statement environment)
+    (update (get-assign-lhs statement) (eval-expression (get-assign-rhs statement) environment) environment))
 
 ; We need to check if there is an else condition.  Otherwise, we evaluate the expression and do the right thing.
-(define interpret-if
-  (lambda (statement environment return break continue throw)
+(define (interpret-if statement environment return break continue throw)
     (cond
       ((eval-expression (get-condition statement) environment) (interpret-statement (get-then statement) environment return break continue throw))
       ((exists-else? statement) (interpret-statement (get-else statement) environment return break continue throw))
-      (else environment))))
+      (else environment)))
 
 ; Interprets a while loop.  We must create break and continue continuations for this loop
-(define interpret-while
-  (lambda (statement environment return throw)
+(define (interpret-while statement environment return throw)
     (call/cc
      (lambda (break)
        (letrec ((loop (lambda (condition body environment)
                         (if (eval-expression condition environment)
                             (loop condition body (interpret-statement body environment return break (lambda (env) (break (loop condition body env))) throw))
                          environment))))
-         (loop (get-condition statement) (get-body statement) environment))))))
+         (loop (get-condition statement) (get-body statement) environment)))))
 
 ; Interprets a block.  The break, continue, and throw continuations must be adjusted to pop the environment
-(define interpret-block
-  (lambda (statement environment return break continue throw)
+(define (interpret-block statement environment return break continue throw)
     (pop-frame (interpret-statement-list (cdr statement)
                                          (push-frame environment)
                                          return
                                          (lambda (env) (break (pop-frame env)))
                                          (lambda (env) (continue (pop-frame env)))
-                                         (lambda (v env) (throw v (pop-frame env)))))))
+                                         (lambda (v env) (throw v (pop-frame env))))))
 
 ; We use a continuation to throw the proper value. Because we are not using boxes, the environment/state must be thrown as well so any environment changes will be kept
-(define interpret-throw
-  (lambda (statement environment throw)
-    (throw (eval-expression (get-expr statement) environment) environment)))
+(define (interpret-throw statement environment throw)
+    (throw (eval-expression (get-expr statement) environment) environment))
 
 ; Interpret a try-catch-finally block
 
 ; Create a continuation for the throw.  If there is no catch, it has to interpret the finally block, and once that completes throw the exception.
 ;   Otherwise, it interprets the catch block with the exception bound to the thrown value and interprets the finally block when the catch is done
-(define create-throw-catch-continuation
-  (lambda (catch-statement environment return break continue throw jump finally-block)
+(define (create-throw-catch-continuation catch-statement environment return break continue throw jump finally-block)
     (cond
       ((null? catch-statement) (lambda (ex env) (throw ex (interpret-block finally-block env return break continue throw))))
       ((not (eq? 'catch (statement-type catch-statement))) (myerror "Incorrect catch statement"))
@@ -116,7 +108,7 @@
                                                  (lambda (env2) (break (pop-frame env2)))
                                                  (lambda (env2) (continue (pop-frame env2)))
                                                  (lambda (v env2) (throw v (pop-frame env2)))))
-                                     return break continue throw)))))))
+                                     return break continue throw))))))
 
 ; To interpret a try block, we must adjust  the return, break, continue continuations to interpret the finally block if any of them are used.
 ;  We must create a new throw continuation and then interpret the try block with the new continuations followed by the finally block with the old continuations
@@ -135,40 +127,35 @@
                           return break continue throw))))))
 
 ; helper methods so that I can reuse the interpret-block method on the try and finally blocks
-(define make-try-block
-  (lambda (try-statement)
-    (cons 'begin try-statement)))
+(define (make-try-block try-statement)
+    (cons 'begin try-statement))
 
-(define make-finally-block
-  (lambda (finally-statement)
+(define (make-finally-block finally-statement)
     (cond
       ((null? finally-statement) '(begin))
       ((not (eq? (statement-type finally-statement) 'finally)) (myerror "Incorrectly formatted finally block"))
-      (else (cons 'begin (cadr finally-statement))))))
+      (else (cons 'begin (cadr finally-statement)))))
 
 ; Evaluates all possible boolean and arithmetic expressions, including constants and variables.
-(define eval-expression
-  (lambda (expr environment)
+(define (eval-expression expr environment)
     (cond
       ((number? expr) expr)
       ((eq? expr 'true) #t)
       ((eq? expr 'false) #f)
       ((not (list? expr)) (lookup expr environment))
-      (else (eval-operator expr environment)))))
+      (else (eval-operator expr environment))))
 
 ; Evaluate a binary (or unary) operator.  Although this is not dealing with side effects, I have the routine evaluate the left operand first and then
 ; pass the result to eval-binary-op2 to evaluate the right operand.  This forces the operands to be evaluated in the proper order in case you choose
 ; to add side effects to the interpreter
-(define eval-operator
-  (lambda (expr environment)
+(define (eval-operator expr environment)
     (cond
       ((eq? '! (operator expr)) (not (eval-expression (operand1 expr) environment)))
       ((and (eq? '- (operator expr)) (= 2 (length expr))) (- (eval-expression (operand1 expr) environment)))
-      (else (eval-binary-op2 expr (eval-expression (operand1 expr) environment) environment)))))
+      (else (eval-binary-op2 expr (eval-expression (operand1 expr) environment) environment))))
 
 ; Complete the evaluation of the binary operator by evaluating the second operand and performing the operation.
-(define eval-binary-op2
-  (lambda (expr op1value environment)
+(define (eval-binary-op2 expr op1value environment)
     (cond
       ((eq? '+ (operator expr)) (+ op1value (eval-expression (operand2 expr) environment)))
       ((eq? '- (operator expr)) (- op1value (eval-expression (operand2 expr) environment)))
@@ -183,7 +170,7 @@
       ((eq? '>= (operator expr)) (>= op1value (eval-expression (operand2 expr) environment)))
       ((eq? '|| (operator expr)) (or op1value (eval-expression (operand2 expr) environment)))
       ((eq? '&& (operator expr)) (and op1value (eval-expression (operand2 expr) environment)))
-      (else (myerror "Unknown operator:" (operator expr))))))
+      (else (myerror "Unknown operator:" (operator expr)))))
 
 ; Determines if two values are equal.  We need a special test because there are both boolean and integer types.
 (define isequal
@@ -233,41 +220,47 @@
     (car (operand1 catch-statement))))
 
 
+(define get-firstelement operator)
+(define get-secondelement operand1)
+
 ;------------------------
 ; Environment/State Functions
 ;------------------------
-
-; create a new empty environment
-(define newenvironment
-  (lambda ()
-    (list (newframe))))
-
-; create an empty frame: a frame is two lists, the first are the variables and the second is the "store" of values
-(define newframe
-  (lambda ()
-    '(() ())))
-
-; add a frame onto the top of the environment
-(define push-frame
-  (lambda (environment)
-    (cons (newframe) environment)))
-
-; remove a frame from the environment
-(define pop-frame
-  (lambda (environment)
-    (cdr environment)))
 
 ; some abstractions
 (define topframe car)
 (define remainingframes cdr)
 
+; Returns the list of variables from a frame
+(define (variables frame)
+  (car frame))
+
+; Returns the values from a frame
+(define (values frame)
+    (cadr frame))
+
+; create an empty frame: a frame is two lists ((variables)(values))
+(define (newframe)
+  '(() ()))
+
+; create a new empty environment
+(define (newenvironment)
+  (list (newframe)))
+
+; add a frame onto the top of the environment
+(define (push-frame environment)
+    (cons (newframe) environment))
+
+; remove a frame from the environment
+(define (pop-frame environment)
+    (cdr environment))
+
 ; does a variable exist in the environment?
-(define exists?
-  (lambda (var environment)
+(define (exists? var environment)
     (cond
       ((null? environment) #f)
       ((exists-in-list? var (variables (topframe environment))) #t)
-      (else (exists? var (remainingframes environment))))))
+      (else (exists? var (remainingframes environment)))))
 
 ; does a variable exist in a list?
 (define exists-in-list?
@@ -291,19 +284,18 @@
           value))))
 
 ; Return the value bound to a variable in the environment
-(define lookup-in-env
-  (lambda (var environment)
+(define (lookup-in-env var environment)
     (cond
       ((null? environment) (myerror "error: undefined variable" var))
       ((exists-in-list? var (variables (topframe environment))) (lookup-in-frame var (topframe environment)))
-      (else (lookup-in-env var (cdr environment))))))
+      (else (lookup-in-env var (cdr environment)))))
 
 ; Return the value bound to a variable in the frame
 (define lookup-in-frame
   (lambda (var frame)
     (cond
       ((not (exists-in-list? var (variables frame))) (myerror "error: undefined variable" var))
-      (else (language->scheme (get-value (indexof var (variables frame)) (store frame)))))))
+      (else (language->scheme (get-value (indexof var (variables frame)) (values frame)))))))
 
 ; Get the location of a name in a list of names
 (define indexof
@@ -321,11 +313,11 @@
       (else (get-value (- n 1) (cdr l))))))
 
 ; Adds a new variable/value binding pair into the environment.  Gives an error if the variable already exists in this frame.
-(define insert
-  (lambda (var val environment)
-    (if (exists-in-list? var (variables (car environment)))
+;returns the new environment
+(define (insert var val environment)
+    (if (exists-in-list? var (variables (topframe environment)))
         (myerror "error: variable is being re-declared:" var)
-        (cons (add-to-frame var val (car environment)) (cdr environment)))))
+        (cons (add-to-frame var val (topframe environment)) (remainingframes environment))))
 
 ; Changes the binding of a variable to a new value in the environment.  Gives an error if the variable does not exist.
 (define update
@@ -335,9 +327,8 @@
         (myerror "error: variable used but not defined:" var))))
 
 ; Add a new variable/value pair to the frame.
-(define add-to-frame
-  (lambda (var val frame)
-    (list (cons var (variables frame)) (cons (scheme->language val) (store frame)))))
+(define (add-to-frame var val frame)
+    (list (cons var (variables frame)) (cons (scheme->language val) (values frame))))
 
 ; Changes the binding of a variable in the environment to a new value
 (define update-existing
@@ -349,24 +340,136 @@
 ; Changes the binding of a variable in the frame to a new value.
 (define update-in-frame
   (lambda (var val frame)
-    (list (variables frame) (update-in-frame-store var val (variables frame) (store frame)))))
+    (list (variables frame) (update-in-frame-values var val (variables frame) (values frame)))))
 
-; Changes a variable binding by placing the new value in the appropriate place in the store
-(define update-in-frame-store
+; Changes a variable binding by placing the new value in the appropriate place in the values
+(define update-in-frame-values
   (lambda (var val varlist vallist)
     (cond
       ((eq? var (car varlist)) (cons (scheme->language val) (cdr vallist)))
-      (else (cons (car vallist) (update-in-frame-store var val (cdr varlist) (cdr vallist)))))))
+      (else (cons (car vallist) (update-in-frame-values var val (cdr varlist) (cdr vallist)))))))
 
-; Returns the list of variables from a frame
-(define variables
-  (lambda (frame)
-    (car frame)))
+; closure is of the form:
+;((function name)(parameter list)(function body)(bindings from cstate when the function was created))
+; environment is of the form:
+;((function1 closure) (function2 closure) ... (global variables))
 
-; Returns the store from a frame
-(define store
-  (lambda (frame)
-    (cadr frame)))
+; abstractions for the dealing with closures
+(define (function-name closure)
+  (car closure))
+(define (function-parameters closure)
+  (cadr closure))
+(define (function-body closure)
+  (caddr closure))
+(define (function-bindings closure)
+  (cadddr closure))
+;TODO merge this with the given mstate solution
+  ;change the state to be {(f1 closure) (f2 closure) (global variables)}
+
+; stores the closure of the function in the current state, and adds it to the top of the environment
+(define (store-function-closure fName paramList fBody environment cstate)
+  (append (list fName paramList fBody (get-current-bindings environment)) cstate))
+
+;global is the function name in the last frame of the state, so same as getting the function 'global
+(define (get-global-variables state)
+  (get-function 'global state))
+
+(define (get-function fname state)
+  (cond
+    ((null? state) (myerror "error: couldn't find function " fname))
+    ((equal? fname (function-name (topframe state))) (topframe state))
+    ;I chose equal so that functions are caps independant, if we want factorial() and Factorial() to be considered the same we need to manipulate the atom fname
+    (else get-function fname (remainingframes state))))
+
+
+
+
+
+
+; returns the parameters evaluated in the current state
+; ex (x y z) for actual-params params returns ((x y z)(1 2 3))+ rest of function environment
+(define (add-bindings formal-params actual-params function-environment state)
+  (if (not(is-compatible-param-list formal-param actual-param state))
+      (myerror "parameter mismatch : formal parameters should be " formal-params)
+      (add-param-bindings formal-params actual-params function-environment state)))
+
+(define (add-param-bindings f-params a-params environment state)
+  (cond
+    ((null? f-params) '())
+    (else (add-param-bindings (cdr f-params) (cdr a-params)(insert (get-firstelement f-param) (eval-expression (get-firstelement a-params) state) environment) state))))
+
+;just checks for length right now, but change is-compatible to check that they're both numbers/boolean/etc
+(define (is-compatible-param-list l1 l2 state)
+  (cond
+    ((and (null? l1) (null? l2)) #t)
+    ((or (null? l1) (null? l2)) #f); if we get here, then one is not null so the length is mismatched, this is here so it returns and doesn't crash
+    ((not (is-compatible? (car l1) (car l2) state)) #f)
+    (else (is-compatible-param-list (cdr l1) (cdr l2) state))))
+
+(define (is-compatible x y state)
+    ;TODO not sure if we need to check for compatibility?
+    #t)
+
+
+
+; all the variable bindings that are active right now
+; (function1 closure) (function2 closure) ... (global variables))
+; returns: ({(vars for function 1)(vals for funciton 1)} {(vars for function2) (f2 vals)}  ... )
+; it needs to be this so that we can have "private" variables that are specific to functions
+(define (get-current-bindings environment)
+  (cond
+    ((null? environment) '())
+    (else ;TODO how to get all the current bindings depends on how we store them in the state
+      ;how do we structure saving the environment?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+; Miscelaneous helper functions
+
+(define call/cc call-with-current-continuation)
 
 ; Functions to convert the Scheme #t and #f to our languages true and false, and back.
 
